fa34005331b96b65ae9eead440da835ff37a729f
CyrilPerrinjaquet <cyrilp.perso@gmail.com>
2023-04-05 09:25:43 +0200

    First commit

---
+++ APP_FILMS_164/database/2_test_connection_bd.py
@@ -0,0 +1,25 @@
+"""Démonstration d'envoi d'une requête SQL à la BD
+Fichier : 2_test_connection_bd.py
+Auteur : OM 2023.03.21
+"""
+
+from APP_FILMS_164.database.database_tools import DBconnection
+
+try:
+    """
+        Une seule requête pour montrer la récupération des données de la BD en MySql.
+    """
+    strsql_genres_afficher = """SELECT id_allergie, nom_allergie, allergene_allergie, gravite_allergie, symptomes_allergie, precautions_allergie, traitement_allergie, notes_allergie FROM t_allergie ORDER BY id_allergie"""
+
+    with DBconnection() as db:
+        db.execute(strsql_genres_afficher)
+        result = db.fetchall()
+        print("data_genres ", result, " Type : ", type(result))
+
+
+except Exception as erreur:
+    # print(f"2547821146 Connection à la BD Impossible ! {type(erreur)} args {erreur.args}")
+    print(f"2547821146 Test connection BD !"
+          f"{__name__,erreur} , "
+          f"{repr(erreur)}, "
+          f"{type(erreur)}")
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Module 164 2023.03.25
+
+
+Le "début de la fin"
+
+
+Le mode d'emploi et vos devoirs se trouvent à l'adresse suivante.
+
+https://info164.github.io/doc164ver1/index.html
